144eed0d7d15479975fb98bcb4befdbd3a4464c4
ZaraMr <zhra.mrdi2907@gmail.com>
2024-01-22 14:49:09 +0330

    Add Aditional JS

--- script.js
+++ script.js
@@ -8,13 +8,28 @@ let newNum;
 decreaseBtn.addEventListener("click", function () {
   newNum = Number(currentNum.innerHTML) - 1;
   currentNum.innerHTML = newNum;
+  console.log(newNum);
+
+  if (newNum <= -1) {
+    currentNum.style.color = "red";
+  } else if (newNum === 0) {
+    currentNum.style.color = "white";
+  }
 });
 
 increaseBtn.addEventListener("click", function () {
   newNum = Number(currentNum.innerHTML) + 1;
   currentNum.innerHTML = newNum;
+  console.log(newNum);
+
+  if (newNum >= 1) {
+    currentNum.style.color = "green";
+  } else if (newNum === 0) {
+    currentNum.style.color = "white";
+  }
 });
 
 resetBtn.addEventListener("click", function () {
   currentNum.innerHTML = 0;
+  currentNum.style.color = "white";
 });
